855bbb53e142731a265aa63f2568c78d8f2affee
rayhan52ve <sajidrayhan875@gmail.com>
2023-04-11 12:32:24 +0600

    react css

--- src/index.js
+++ src/index.js
@@ -7,13 +7,23 @@ const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
+
+// const headingStyle = {
+//     backgroundColor : "purple",
+//     color : "white",
+//     textAlign : "center",
+//     padding : "15px"
+// };
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div>
-     <h1>Todo App</h1>
-     <h3>{todoTitle}</h3>
-     <p>{todoDesc}</p>
-     <p>{dateName + "/" + monthName + "/" + currentYear}</p>
+    <div className='rootBody'>
+     <h1 className="headingStyle largeText">Todo App</h1>
+     <div className='card'>
+     <h3 className='cardTitle' >{todoTitle}</h3>
+     <p className='cardDesc' >{todoDesc}</p>
+     <p className='cardFooter' >{dateName + "/" + monthName + "/" + currentYear}</p>
+     </div>
     </div>
 );
 
