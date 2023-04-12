98e4d42dc7f7a346bb6902b777fac238429b9a36
rayhan52ve <sajidrayhan875@gmail.com>
2023-04-12 16:16:43 +0600

    props & destructing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Card from "./components/Card";
 function App(){
    return <div className='rootBody'>
      <h1 className="headingStyle largeText">Todo App</h1>
-       <Card />
-       <Card />
-       <Card />
-       <Card />
+       <Card todoTitle = "call mother" todoDesc = "This is desc1" />
+       <Card todoTitle = "call father" todoDesc = "This is desc2" />
+       <Card todoTitle = "call wife" todoDesc = "This is desc3"/>
     </div>
 }
 
